2e911b03c60dcff895911ae9b1946fd378a93b44
DianaVaror <dianitavaror@gmail.com>
2023-05-27 09:00:52 -0500

    Calculator js code

--- js/calculator.js
+++ js/calculator.js
@@ -8,3 +8,32 @@ const botonBorrarTodo = document.querySelector('[data-borrar-todo]')
 const botonBorrar = document.querySelector('[data-borrar]')
 const textoValorSuperior = document.querySelector('[data-valor-superior]')
 const textoValorInferior = document.querySelector('[data-valor-inferior]')
+
+
+class Calculadora {
+    constructor(textoValorInferior, textoValorSuperior){
+        this.textoValorInferior = textoValorInferior
+        this.textoValorSuperior = textoValorSuperior
+        this.ValorInferior = ''
+        this.ValorSuperior = ''
+        this.operador = undefined
+    }
+
+    agregarNumero(numero) {
+    this.valorInferior = this.valorInferior + numero
+    }
+    imprimirDisplay() {
+        this.textoValorInferior.innerText = this.valorInferior
+    }
+}
+
+const calculadora = new Calculadora (textoValorInferior, textoValorSuperior)
+
+
+botonNumero.forEach(boton => {
+    boton.addEventListener('click', () => {
+        calculadora.agregarNumero(boton.innerText)
+        calculadora.imprimirDisplay()
+    })
+    
+})
