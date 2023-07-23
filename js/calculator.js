689bd97ab4dbb9cc5041f252e8eca5caea1391ca
DianaVaror <dianitavaror@gmail.com>
2023-07-23 08:39:56 -0500

    Calculator with new design

--- js/calculator.js
+++ js/calculator.js
@@ -1,39 +1,31 @@
-//lo primero que hacemos es crear una variable o constante con los valores que va a tener html para que javascript los pueda utilizar
-//creamos una constante que va a utilizar un metodo que nos va a traer todos los elementos que tenga data  de html y asi js los pueda reconocer
+let expression = '';
 
-const botonNumero = document.querySelectorAll('[data-numero]')
-const botonOperador = document.querySelectorAll('[data-operador]')
-const botonIgual = document.querySelector('[data-igual]')
-const botonBorrarTodo = document.querySelector('[data-borrar-todo]')
-const botonBorrar = document.querySelector('[data-borrar]')
-const textoValorSuperior = document.querySelector('[data-valor-superior]')
-const textoValorInferior = document.querySelector('[data-valor-inferior]')
-
-
-class Calculadora {
-    constructor(textoValorInferior, textoValorSuperior){
-        this.textoValorInferior = textoValorInferior
-        this.textoValorSuperior = textoValorSuperior
-        this.ValorInferior = ''
-        this.ValorSuperior = ''
-        this.operador = undefined
-    }
-
-    agregarNumero(numero) {
-    this.valorInferior = this.valorInferior + numero
-    }
-    imprimirDisplay() {
-        this.textoValorInferior.innerText = this.valorInferior
-    }
+function appendToDisplay(value) {
+  expression += value;
+  document.getElementById('display').value = expression;
 }
 
-const calculadora = new Calculadora (textoValorInferior, textoValorSuperior)
+function clearDisplay() {
+  expression = '';
+  document.getElementById('display').value = '';
+}
 
+function calculate() {
+  try {
+    const result = eval(expression);
+    expression = result.toString();
+    document.getElementById('display').value = result;
+  } catch (error) {
+    document.getElementById('display').value = 'Error';
+  }
+}
 
-botonNumero.forEach(boton => {
-    boton.addEventListener('click', () => {
-        calculadora.agregarNumero(boton.innerText)
-        calculadora.imprimirDisplay()
-    })
-    
-})
+function calculateSquareRoot() {
+  try {
+    const result = Math.sqrt(eval(expression));
+    expression = result.toString();
+    document.getElementById('display').value = result;
+  } catch (error) {
+    document.getElementById('display').value = 'Error';
+  }
+}
